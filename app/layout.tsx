46ee70534323894894560e857d8375d32e1ea231
yafie <m.yafie.dalimunthe@gmail.com>
2024-03-08 10:35:13 +0700

    First Init

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "App for showing pokemon list and detail",
+  description: "only for frontend test purpose",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
